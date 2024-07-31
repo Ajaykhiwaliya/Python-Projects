8cd55fa22cdfe9068326ea2543d8fa7927c97afc
Ajaykhiwaliya <165060553+Ajaykhiwaliya@users.noreply.github.com>
2024-07-31 16:15:53 +0530

    Create Quiz.py

---
+++ Quiz.py
@@ -0,0 +1,33 @@
+print("Welcome to my computer qiuz")
+playing= input("Do you want to play ? ")
+
+if playing != "yes":
+    quit()
+
+    print("Okay! Let's play: )")
+
+    answer = input("What does CPU stand for? ")
+    if answer == "central processing unit":
+        print('Correct!')
+    else:
+            print("Incorrect")
+            answer = input("What does GPU stand for? ")
+    if answer == "graphics processing unit":
+        print('Correct!')
+    else:
+            print("Incorrect")
+            answer = input("What does RAM stand for? ")
+    if answer == "random access memory":
+        print('Correct!')
+    else:
+            print("Incorrect")
+            answer = input("What does PSU stand for? ")
+    if answer == "power supply unit":
+        print('Correct!')
+    else:
+            print("Incorrect")
+            answer = input("What does ROM stand for? ")
+    if answer == "read only memory":
+        print('Correct!')
+    else:
+            print("Incorrect")

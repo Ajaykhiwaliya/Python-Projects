7ee57fe2a6da8e87568f9a0c6f57767a5681fee2
Ajaykhiwaliya <165060553+Ajaykhiwaliya@users.noreply.github.com>
2024-07-31 16:26:11 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,33 +1,41 @@
 print("Welcome to my computer qiuz")
 playing= input("Do you want to play ? ")
 
-if playing != "yes":
+if playing.lower()!= "yes":
     quit()
 
     print("Okay! Let's play: )")
+    score = 0
 
-    answer = input("What does CPU stand for? ")
+    answer.lower() = input("What does CPU stand for? ")
     if answer == "central processing unit":
         print('Correct!')
+        score +=1
     else:
             print("Incorrect")
-            answer = input("What does GPU stand for? ")
+            answer.lower() = input("What does GPU stand for? ")
     if answer == "graphics processing unit":
         print('Correct!')
+        score +=1
     else:
             print("Incorrect")
-            answer = input("What does RAM stand for? ")
+            answer.lower() = input("What does RAM stand for? ")
     if answer == "random access memory":
         print('Correct!')
+        score +=1
     else:
             print("Incorrect")
-            answer = input("What does PSU stand for? ")
+            answer.lower() = input("What does PSU stand for? ")
     if answer == "power supply unit":
         print('Correct!')
+        score +=1
     else:
             print("Incorrect")
-            answer = input("What does ROM stand for? ")
+            answer.lower() = input("What does ROM stand for? ")
     if answer == "read only memory":
         print('Correct!')
+        score +=1
     else:
-            print("Incorrect")
+        print("Incorrect")
+    print("You got " + str(score) + " questions correct !")
+    print("You got " + str((score / 4) * 100) + "%.")

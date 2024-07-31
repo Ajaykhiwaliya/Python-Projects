a9a05a87bd0b118b1de304d0145707c264099be8
Ajaykhiwaliya <165060553+Ajaykhiwaliya@users.noreply.github.com>
2024-07-31 20:49:07 +0530

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,41 +1,41 @@
 print("Welcome to my computer qiuz")
 playing= input("Do you want to play ? ")
 
-if playing.lower()!= "yes":
+if playing != "yes":
     quit()
+print("Okay! Let's play: )")
+score = 0
+answer = input("What does CPU stand for? ")
+if answer.lower() == "central processing unit":
+        print('Correct!')
+        score +=1
+else:
+        print('Incorrect!')
+answer = input("What does GPU stand for? ")
+if answer.lower() == "graphics processing unit":
+        print('Correct!')
+        score +=1
+else:
+            print("Incorrect")
+            answer = input("What does RAM stand for? ")
+if answer.lower() == "random access memory":
+     print('Correct')
+     score +=1
 
-    print("Okay! Let's play: )")
-    score = 0
+else:
+    print('Incorrect')
 
-    answer.lower() = input("What does CPU stand for? ")
-    if answer == "central processing unit":
+answer = input("What does PSU stand for? ")
+if answer.lower() == "power supply unit":
         print('Correct!')
         score +=1
-    else:
-            print("Incorrect")
-            answer.lower() = input("What does GPU stand for? ")
-    if answer == "graphics processing unit":
+else:
+            print('Incorrect')
+            answer = input("What does ROM stand for? ")
+if answer.lower() == "read only memory":
         print('Correct!')
         score +=1
-    else:
-            print("Incorrect")
-            answer.lower() = input("What does RAM stand for? ")
-    if answer == "random access memory":
-        print('Correct!')
-        score +=1
-    else:
-            print("Incorrect")
-            answer.lower() = input("What does PSU stand for? ")
-    if answer == "power supply unit":
-        print('Correct!')
-        score +=1
-    else:
-            print("Incorrect")
-            answer.lower() = input("What does ROM stand for? ")
-    if answer == "read only memory":
-        print('Correct!')
-        score +=1
-    else:
-        print("Incorrect")
-    print("You got " + str(score) + " questions correct !")
+else:
+      print("Incorrect")
+print("You got " + str(score) + " questions correct!")
     print("You got " + str((score / 4) * 100) + "%.")
